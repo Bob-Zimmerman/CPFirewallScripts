b4ba0291c4b87aaf874803b70fdecc9a0fb3de65
Bob-Zimmerman <63750975+Bob-Zimmerman@users.noreply.github.com>
2025-02-25 15:22:23 -0600

    Updating scanInts.sh to work on current versions, including VSX

--- scanInts.sh
+++ scanInts.sh
@@ -1,16 +1,19 @@
 #!/bin/env bash
-################################################################################
-## This script iterates through all of the interfaces on a firewall and does an
-## ARP sweep of each one (using ping to trigger ARP). It then reports which
-## interfaces' networks are empty. It looks at all interfaces with names which
-## do not start with 'lo' or 'wrp'.
+########################################################################
+## This script iterates through all of the interfaces on a firewall and
+## does an ARP sweep of each one (using ping to trigger ARP). It then
+## reports which interfaces' networks are empty. It looks at all
+## interfaces with names which do not start with 'lo', 'wrp', 'gre0' or
+## 'gretap0'.
 ## 
-## The Linux network kernel has a terrible limitation which necessitates the
-## 'sleep 300s' in scanInterfaces. Specifically, there is no good way to
-## manually *delete* ARP cache entries. You can only invalidate them. They stay
-## around as 'incomplete', cluttering up the table. If we don't slow down, they
-## eventually fill the table and prevent new entries from being added.
+## The Linux network kernel has a terrible limitation which necessitates
+## the 'sleep 300s' in scanInterfaces. Specifically, there is no good
+## way to manually *delete* ARP cache entries. You can only invalidate
+## them. They stay around as 'incomplete', cluttering up the table. If
+## we don't slow down, they eventually fill the table and prevent new
+## entries from being added.
 ################################################################################
+. /etc/profile.d/vsenv.sh
 
 ## First, we build some tables. No reason to compute this at runtime.
 maskFromLength=(0x0
@@ -83,85 +86,67 @@ addressCountFromLength=(4294967294
 
 dottedToNumber()
 	{
-	## This function accepts an IP in dotted decimal form and returns a raw
-	## number. It's possible to do math with numbers, and nightmarish to do it
-	## with dotted decimal IP addresses.
+	## Convert an IP in dotted decimal form to a raw number. It's
+	## possible to do math with numbers, and nightmarish to do it with
+	## dotted decimal IP addresses.
 	first=$(echo $1 | cut -d. -f1)
 	second=$(echo $1 | cut -d. -f2)
 	third=$(echo $1 | cut -d. -f3)
 	fourth=$(echo $1 | cut -d. -f4)
-	echo "$(( $first*2**24 + $second*2**16 + $third*2**8 + $fourth ))"
+	echo "$(( (${first}<<24) + (${second}<<16) + (${third}<<8) + ${fourth} ))"
 	}
 
 scanNetwork()
 	{
 	## This function accepts an IP address and prefix length in the form
-	## "10.20.30.40/24". It figures out the usable addresses in the block, then
-	## iterates through it, pinging them, waiting briefly, then checking the
-	## ARP table to see if something responded. It then prints the number of
-	## addresses which responded to ARP within the network along with the
-	## interface's name, IP/prefix, and (if the system has more than one VRF),
-	## the VRF the interface belongs to.
+	## "10.20.30.40/24". It figures out the usable addresses in the
+	## block, iterates through it, pings each one, waits briefly, then
+	## checks the ARP table to see if something responded. It returns
+	## the number of responses it got.
 	ipAddr=$(dottedToNumber "$(echo $1 | cut -d/ -f1)")
 	maskLength=$(echo $1 | cut -d/ -f2)
-	lowest=$(((ipAddr&${maskFromLength[$maskLength]})))
+	lowest=$((($ipAddr&${maskFromLength[$maskLength]})))
 	highest=$(($lowest+${addressCountFromLength[$maskLength]}))
 	itemsInNetwork=0
 	for scanAddress in $(seq -f "%.0f" $(($lowest+1)) $highest)
 		do
 		ping -c 1 -w 1 $scanAddress 2>&1 >/dev/null &
 		sleep 0.02s
-		if [ $(arp -n $scanAddress | tail -n 1 | awk '{print $2}') != "(incomplete)" ]; then
+		if [ "" != "$(arp -n $scanAddress | egrep -v "(incomplete|no entry|HWaddress)")" ];then
 			((itemsInNetwork+=1))
 		fi
 		done
-	echo -n "$itemsInNetwork items in $interfaceToScan $1"
-	if [ -d /proc/vrf ] && [ $(ls /proc/vrf/ | wc -l) -gt 1 ]; then
-		echo -n " in VRF $(cat /proc/self/vrf)"
-	fi
-	echo ""
+		echo "${itemsInNetwork}"
 	}
 
 scanInterfaces()
 	{
-	## This function accepts no arguments. It runs in the current VRF, finds
-	## every interface which doesn't start with 'lo' or 'wrp', and feeds them
-	## into 'scanNetwork'. The 'sleep 300s' after the 'scanNetwork' call is to
-	## work around the Linux network stack's desire to hold on to ARP cache
-	## entries, even if the admin says to delete them.
-	for interfaceToScan in $(ip link show | egrep "^[0-9]" | cut -d" " -f2 | cut -d@ -f1 | sed 's#:##' | egrep -v "^(lo[0-9]*|wrp)" | sort)
+	## This function accepts no arguments. It runs in the current VRF,
+	## finds every interface which doesn't start with 'lo', 'wrp',
+	## 'gre0', or 'gretap0', and feeds them into 'scanNetwork'. The
+	## 'sleep 300s' is to work around the Linux network stack's desire
+	## to hold on to ARP cache entries, even if the admin says to delete
+	## them.
+	vsid="$(sed -E 's/^0$//' /proc/self/nsid)"
+	for interfaceToScan in $(ip link show | egrep "^[0-9]" | cut -d" " -f2 | cut -d@ -f1 | sed 's#:##' | egrep -v "^(lo[0-9]*|wrp|gre0|gretap0)" | sort)
 		do
-		rawAddr=$(/sbin/ip addr show $interfaceToScan | grep inet | awk '{print $2}')
-		if [ "$rawAddr" != "" ]; then
-			scanNetwork "$rawAddr"
-			sleep 300s
-		else
-			echo -n "$interfaceToScan"
-			if [ -d /proc/vrf ] && [ $(ls /proc/vrf/ | wc -l) -gt 1 ]; then
-				echo -n " in VRF $(cat /proc/self/vrf)"
+		## Get the local interface routes.
+		routes=$(ip route show | grep " ${interfaceToScan} " | grep -v "via" | cut -d' ' -f1)
+		if [ "" = "${routes[@]}" ];then
+			# If there are no local routes for this interface, bail early.
+			echo >&2 "${interfaceToScan}${vsid:+ in NSID $vsid} has no IP address. Skipping."
+			continue
 			fi
-			echo " has no IP address. Skipping."
-		fi
+		for rawAddr in ${routes[@]};do
+			items="$(scanNetwork "${rawAddr}")"
+			echo "$items other items in ${rawAddr} on ${interfaceToScan}${vsid:+ in NSID $vsid}"
+			done
+		sleep 300s
 		done
 	}
 
-scanVRFs()
-	{
-	## This function accepts no arguments. It lists all of the VRFs, switches
-	## into them, and calls 'scanInterfaces'. This can be skipped if there is
-	## only the one routing table.
-	for vrfToScan in $(ls /proc/vrf/ | sort -n)
-		do
-		vsx set $vrfToScan>/dev/null
-		scanInterfaces
-		sleep 10s
-		done
-	}
-
-## If this system is VRF-aware and we're running more than one VRF, scan the
-## VRFs. Otherwise, just scan the interfaces for the current VRF.
-if [ -d /proc/vrf ] && [ $(ls /proc/vrf/ | wc -l) -gt 1 ]; then
-	scanVRFs
-else
+for context in $(ip netns list | cut -d' ' -f1 | sort);do
+	vsid=$(echo "${context}" | sed -E "s/CTX0+//")
+	vsenv "${vsid}" 2>&1 >/dev/null
 	scanInterfaces
-fi
+	done
