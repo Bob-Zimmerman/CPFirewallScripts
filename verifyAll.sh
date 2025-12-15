b49a2f42488ee87f7a4d4eea80d36e4e2f5fd010
Bob-Zimmerman <63750975+Bob-Zimmerman@users.noreply.github.com>
2025-12-15 16:11:41 -0600

    Cleaning up showAll and cmaList
    Making a few changes recommended by shellcheck

--- verifyAll.sh
+++ verifyAll.sh
@@ -10,33 +10,32 @@ portNumber=$(api status | grep "APACHE Gaia Port" | awk '{print $NF}')
 
 showAll() {
 IFS=$(printf "\377")
-sharedArguments=( --port ${portNumber} -f json ${cmaAddress:+-d} ${cmaAddress:+${cmaAddress}} -r true show "${1}" details-level full limit 500 )
-firstResult=$(mgmt_cli ${sharedArguments[@]})
-if [ "${?}" != "0" ];then return 1;fi
+sharedArguments=( --port "${portNumber}" -f json ${cmaAddress:+-d "${cmaAddress}"} -r true show "${1}" details-level full limit 500 )
+if ! firstResult=$(mgmt_cli "${sharedArguments[@]}");then return 1;fi
 toReturn="$(<<<"${firstResult}" jq -c '.objects[]|.')
 ";objectCount=$(<<<"${firstResult}" jq -c '.total')
 if [ "${objectCount}" -lt 501 ];then echo -n "${toReturn}";return 0;fi
 for offsetVal in $(seq 500 500 "${objectCount}" 2>/dev/null | tr "\n" "${IFS}");do
-toReturn+="$(mgmt_cli ${sharedArguments[@]} offset "${offsetVal}" \
+toReturn+="$(mgmt_cli "${sharedArguments[@]}" offset "${offsetVal}" \
 | jq -c '.objects[]|.')
 ";done;echo -n "${toReturn}";}
 
 cmaList=$(showAll domains \
 | jq -c '{name:.name,server:.servers[]|{host:."multi-domain-server",ipAddress:."ipv4-address"}}' \
-| grep $(hostname) \
+| grep "$(hostname)" \
 | jq -c '[.name,.server.ipAddress]')
-if [ "${#cmaList}" == "0" ];then cmaList=("[\"$(hostname)\",\"\"]");fi
+if [ "0" == "${#cmaList}" ];then cmaList=("[\"$(hostname)\",\"\"]");fi
 
-for cmaRow in $cmaList; do
+echo "${cmaList[@]}" | while read -r cmaRow; do
 cmaName=$(<<<"${cmaRow}" jq '.[0]' | sed 's#"##g')
 cmaAddress=$(<<<"${cmaRow}" jq '.[1]' | sed 's#"##g')
-policyList=$(mgmt_cli --port ${portNumber} -f json ${cmaAddress:+-d} ${cmaAddress:+${cmaAddress}} -r true show packages limit 500 \
+policyList=$(mgmt_cli --port "${portNumber}" -f json ${cmaAddress:+-d "${cmaAddress}"} -r true show packages limit 500 \
 | jq -c '.packages[]|.name' \
 | tr -d '"')
 for policy in $policyList; do
 echo "=========================================================="
 printf "%-16s %30s: " "${cmaName}" "${policy}"
-tasks=$(mgmt_cli -f json -d "${cmaAddress}" -r true verify-policy policy-package ${policy} 2>/dev/null \
+tasks=$(mgmt_cli -f json -d "${cmaAddress}" -r true verify-policy policy-package "${policy}" 2>/dev/null \
 | jq -c '.tasks[]')
 status=$(<<<"${tasks}" jq -c '.status' | tr -d '"')
 case "${status}" in
